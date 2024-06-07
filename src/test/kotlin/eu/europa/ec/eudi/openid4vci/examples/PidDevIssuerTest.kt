e7f1957ba4815a8fee59530ec9a7d6f41647b524
babisRoutis <haralampos.routis@netcompany.com>
2024-06-07 18:18:23 +0300

    Code format

--- src/test/kotlin/eu/europa/ec/eudi/openid4vci/examples/PidDevIssuerTest.kt
+++ src/test/kotlin/eu/europa/ec/eudi/openid4vci/examples/PidDevIssuerTest.kt
@@ -18,7 +18,6 @@ package eu.europa.ec.eudi.openid4vci.examples
 import eu.europa.ec.eudi.openid4vci.ProofTypeMetaPreference
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.DisplayName
-import kotlin.test.Ignore
 import kotlin.test.Test
 
 @DisplayName("PID DEV Issuer Test")
